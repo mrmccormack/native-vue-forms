20fd3058ab8427efecb7bf4860ce1ac064b3064b
Signaturr <mysignaturr@gmail.com>
2019-02-03 09:48:24 -0500

    Initial commit

---
+++ app/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+    <Page class="page">
+        <ActionBar title="JSON example" class="action-bar" />
+        <RadDataForm :source="album" :metadata="metadata" />
+    </Page>
+</template>
+
+<script>
+    import Vue from "nativescript-vue";
+    import RadDataForm from "nativescript-ui-dataform/vue";
+    Vue.use(RadDataForm);
+
+    export default {
+        data() {
+            return {
+                album: {
+                    bandName: "Beatles",
+                    albumName: "Seargent Peppers",
+                    year: "2017",
+                    owned: true,
+                    borrowed: true
+                },
+                metadata: {
+                    "isReadOnly": false,
+                    "propertyAnnotations": [{
+                            "name": "bandName",
+                            "index": 0,
+                            "displayName": "Name of Band",
+                            hintText: "hint text here"
+                        },
+                        {
+                            "name": "albumName",
+                            "index": 1
+                        },
+                        {
+                            "name": "year",
+                            "index": 2
+                        },
+                        {
+                            "name": "owned",
+                            "index": 4
+                        },
+                        {
+                            "name": "myRating",
+                            "index": 3
+                        }
+                    ]
+                }
+            };
+        }
+    };
+</script>
