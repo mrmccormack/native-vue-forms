bd17d0e134262eb8c45b9b6de0414a37b1a49633
Signaturr <mysignaturr@gmail.com>
2019-02-03 10:36:51 -0500

    changes...

--- app/components/HelloWorld.vue
+++ app/components/HelloWorld.vue
@@ -1,57 +1,71 @@
 <template>
-    <Page class="page">
-        <ActionBar title="NativeScript/Vue.js - Forms" class="action-bar" />
-        <RadDataForm :source="album" :metadata="metadata" />
-    </Page>
+  <Page class="page">
+    <ActionBar title="NativeScript/Vue.js - Forms" class="action-bar"/>
+    <RadDataForm :source="employees" :metadata="metadata"/>
+  </Page>
 </template>
 
 <script>
-
 /*
 Ref: http://docs.telerik.com/devtools/nativescript-ui/Controls/NativeScript/DataForm/Validation/dataform-validation-overview#json-metadata
 
 */
-    import Vue from "nativescript-vue";
-    import RadDataForm from "nativescript-ui-dataform/vue";
-    Vue.use(RadDataForm);
+import Vue from "nativescript-vue";
+import RadDataForm from "nativescript-ui-dataform/vue";
+Vue.use(RadDataForm);
 
-    export default {
-        data() {
-            return {
-                album: {
-                    bandName: "Beatles",
-                    albumName: "Seargent Peppers",
-                    year: "2017",
-                    owned: true,
-                    borrowed: true
-                },
-                metadata: {
-                    "isReadOnly": false,
-                    "propertyAnnotations": [{
-                            "name": "bandName",
-                            "index": 0,
-                            "displayName": "Name of Band",
-                            hintText: "hint text here"
-                        },
-                        {
-                            "name": "albumName",
-                            "index": 1
-                        },
-                        {
-                            "name": "year",
-                            "index": 2
-                        },
-                        {
-                            "name": "owned",
-                            "index": 4
-                        },
-                        {
-                            "name": "myRating",
-                            "index": 3
-                        }
-                    ]
-                }
-            };
-        }
+export default {
+  data() {
+    return {
+      employees: {
+        userId: "123",
+        jobTitleName: "Developer",
+        firstName: "Fred",
+        lastName: "Smith",
+        city: "",
+        phoneNumber: "408-123-4567",
+        emailAddress: "fredsmith234@gmail.com"
+      },
+
+      metadata: {
+        isReadOnly: false,
+        propertyAnnotations: [
+          {
+            name: "userId",
+            index: 0,
+            displayName: "User Id:",
+            hintText: "hint text here"
+          },
+          {
+            name: "jobTitleName",
+            index: 1
+          },
+          {
+            name: "firstName",
+            index: 2,
+            imageResource: "https://cdn3.iconfinder.com/data/icons/woothemesiconset/32/user.png"
+          },
+          {
+            name: "lastName",
+            index: 3,
+          },
+          {
+            name: "phoneNumber",
+            index: 5
+          },
+          {
+            name: "emailAddress",
+            index: 4
+          },
+          {
+            name: "city",
+            index: 6,
+            "editor": "Picker",
+            "valuesProvider": ["New York", "Washington", "Los Angeles"]
+          },
+        ]
+      }
     };
+  }
+};
 </script>
